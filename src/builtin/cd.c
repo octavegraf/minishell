d968ad21564c3983a3959ecbf3b4eb48edea9172
Octave Graf <ocgraf@student.42.fr>
2025-08-07 15:02:08 +0200

    CD now also modify env variable "OLDPWD"
    Co-authored-by: Linoa <LinoaSaga@users.noreply.github.com>

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 14:23:31 by ljudd             #+#    #+#             */
-/*   Updated: 2025/08/06 16:56:37 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/08/07 15:01:35 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,26 @@ int	mini_cd(char *prompt, t_env *env)
 {
 	char	path[1024];
 	t_env	*pwd_env;
+	t_env	*old_pwd;
+	char	*old_pwd_value;
 
-	if (search_env(env, "HOME") == NULL)
+	old_pwd = search_env(env, "OLDPWD");
+	if (!old_pwd)
+		return (ft_dprintf(2, "cd: OLDPWD not set\n"), 1);
+	old_pwd_value = ft_strdup(old_pwd->value);
+	if (!old_pwd_value)
+		return (perror("malloc"), 1);
+	if (!search_env(env, "HOME"))
 		ft_dprintf(2, "cd: HOME not set\n");
 	if (chdir(prompt) == -1)
-	{
-		perror("cd");
-		return (1);
-	}
+		return (perror("cd"), 1);
 	if (!getcwd(path, 1024))
-	{
-		perror("cd");
-		return (1);
-	}
+		return (perror("cd"), 1);
 	pwd_env = search_env(env, "PWD");
 	if (pwd_env)
 		modify_env(pwd_env, NULL, path);
+	modify_env(old_pwd, NULL, old_pwd_value);
+	free(old_pwd_value);
 	return (0);
 }
 
@@ -51,7 +55,9 @@ int	mini_cd(char *prompt, t_env *env)
 	pwd_env = search_env(env, "PWD");
 	if (pwd_env)
 		ft_printf("Current directory: %s\n", pwd_env->value);
+	pwd_env = search_env(env, "OLDPWD");
+	if (pwd_env)
+		ft_printf("Previous directory: %s\n", pwd_env->value);
 	delete_all_env(env);
 	return (0);
-}
- */
+} */
