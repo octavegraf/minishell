a2a68700f2c56d08722588e9519f9dcf6dcc4e4a
Octave Graf <ocgraf@student.42.fr>
2025-08-13 14:33:15 +0200

    added data file for structure data

---
+++ src/exec/data.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   data.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 11:48:37 by ocgraf            #+#    #+#             */
+/*   Updated: 2025/08/13 11:52:46 by ocgraf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+/* t_data	*data_init(char **envp, char *inputs)
+{
+	data->env = get_env(envp);
+	if (!data->env)
+	{
+		free(data);
+		return (NULL);
+	}
+	data.exit_code = 0;
+	data.inputs = inputs;
+	data.error_parse = NULL;
+	data.token = token;
+	data->pid = 0;
+
+}
+ */
