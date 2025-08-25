4fafa5d0da138f82266f73919e0541ad89477278
Octave Graf <ocgraf@student.42.fr>
2025-08-25 16:22:18 +0200

    is_builtin return if a function should be executed in child or parent

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 17:10:08 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/08/24 17:21:46 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/08/25 09:58:31 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 int	is_builtin(t_cmd *cmd)
 {
 	if (!ft_strcmp(cmd->cmd_path, "cd")
-		|| !ft_strcmp(cmd->cmd_path, "echo")
-		|| !ft_strcmp(cmd->cmd_path, "env")
 		|| !ft_strcmp(cmd->cmd_path, "exit")
 		|| !ft_strcmp(cmd->cmd_path, "export")
-		|| !ft_strcmp(cmd->cmd_path, "pwd")
 		|| !ft_strcmp(cmd->cmd_path, "unset"))
 		return (1);
+	else if (!ft_strcmp(cmd->cmd_path, "echo")
+		|| !ft_strcmp(cmd->cmd_path, "env")
+		|| !ft_strcmp(cmd->cmd_path, "pwd"))
+		return (2);
 	return (0);
 }
 
