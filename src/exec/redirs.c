4a124113d1321e4d7e36ee5f94522ed3df6f7bc6
Octave Graf <ocgraf@student.42.fr>
2025-08-27 17:16:07 +0200

    added redirs function

--- src/exec/redirs.c
+++ src/exec/redirs.c
@@ -6,9 +6,22 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 17:45:17 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/08/24 11:22:39 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/08/27 17:15:56 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/exec.h"
+
+int	redirs(t_cmd cmd)
+{
+	while (cmd.cmd_path)
+	{
+		if (cmd.redirs)
+		{
+			if (exec_redirs(cmd))
+				return (1);
+		}
+	}
+	return (0);
+}
 
