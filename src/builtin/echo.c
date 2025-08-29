005209ceda30311716571a8b6f0185cf30069881
Octave Graf <cobra.morilles-0n@icloud.com>
2025-08-29 11:48:10 +0200

    Rewrite echo.

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,57 +6,36 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 14:21:51 by ljudd             #+#    #+#             */
-/*   Updated: 2025/08/26 17:46:51 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/08/29 11:47:07 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/builtin.h"
 
-int	disable_nl(char *prompt)
-{	
-	int	i;
-
-	i = 1;
-	while (prompt[++i])
-	{
-		if (prompt[i] != 'n')
-			return (0);
-	}
-	return (1);
-}
-
 int	mini_echo(char **args)
 {
 	int	i;
 	int	nl;
-	int	to_print;
 
 	i = -1;
 	nl = 1;
 	while (args[++i])
 	{
-		to_print = 1;
-		if (!ft_strncmp(args[i], "-n", 2))
-		{
-			if (disable_nl(args[i]))
-			{
-				nl = 0;
-				to_print = 0;
-			}
-		}
-		if (i != 0 && args[i] && to_print)
+		if ((i == 1 && nl == 1) || i > 1)
 			write(1, " ", 1);
-		if (to_print)
-			printf("%s", args[i]);
+		if (!i && !ft_strcmp(args[i], "-n"))
+			nl = 0;
+		else
+			write(1, args[i], ft_strlen(args[i]));
 	}
 	if (nl)
 		write(1, "\n", 1);
 	return (0);
 }
 
-/* int	main(int argc, char **argv)
-{
-	mini_echo(argv + 1);
-	(void)argc;
-	return (0);
-} */
+// int	main(int argc, char **argv)
+// {
+// 	mini_echo(argv + 1);
+// 	(void)argc;
+// 	return (0);
+// }
