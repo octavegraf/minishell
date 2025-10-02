67b21b087cdd6f85fdc83c7fa4959e799021f6e8
Octave Graf <ocgraf@student.42.fr>
2025-10-02 11:45:10 +0200

    echo no longer starts to write on the first argument (echo), and now works correctly.

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 14:21:51 by ljudd             #+#    #+#             */
-/*   Updated: 2025/08/29 11:47:07 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/10/02 11:43:34 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,21 @@ int	mini_echo(char **args)
 	int	i;
 	int	nl;
 
-	i = -1;
+	i = 1;
 	nl = 1;
-	while (args[++i])
+	if (args[1] && !ft_strcmp(args[1], "-n"))
 	{
-		if ((i == 1 && nl == 1) || i > 1)
+		nl = 0;
+		i = 2;
+	}
+	while (args[i])
+	{
+		if (nl && i > 1)
 			write(1, " ", 1);
-		if (!i && !ft_strcmp(args[i], "-n"))
-			nl = 0;
-		else
-			write(1, args[i], ft_strlen(args[i]));
+		else if (!nl && i > 2)
+			write(1, " ", 1);
+		write(1, args[i], ft_strlen(args[i]));
+		i++;
 	}
 	if (nl)
 		write(1, "\n", 1);
